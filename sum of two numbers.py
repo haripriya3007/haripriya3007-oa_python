65c7b01ea05985df504606667e4c57d61196a8ad
haripriya3007 <113535880+haripriya3007@users.noreply.github.com>
2022-09-16 07:44:46 -0700

    Add files via upload

---
+++ sum of two numbers.py
@@ -0,0 +1,20 @@
+#sum of two numbers
+x=int(input("enter the number"))
+y=int(input("enter the number"))
+z=x+y
+print(z)
+
+x=int(input("enter the number"))
+y=int(input("enter the number"))
+z=x+y
+print(f"{x}+{y}={z}")
+
+x=input("enter the number")
+y=input("enter the number")
+sum=int(x) + int(y)
+print("the sum is",sum)
+
+x=int(input("quantity"))
+y=int(input("price"))
+z=x*y
+print(f"{x}*{y}={z}")
