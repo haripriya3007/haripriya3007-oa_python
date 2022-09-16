dfaf1fb228bdab56e6d74c9b918a50fdac20e248
haripriya3007 <113535880+haripriya3007@users.noreply.github.com>
2022-09-16 07:46:48 -0700

    Add files via upload

---
+++ python comparison operators 3.py
@@ -0,0 +1,24 @@
+#python Comparison operators
+x=5
+y=8
+print(x<y)
+
+x=5
+y=5
+print(x==y)
+
+x=7
+y=4
+print(x<y)
+
+x=7
+y=9
+print(x<=y)
+
+x=7
+y=9
+print(x>=y)
+
+x=5
+y=3
+print(x!=y)
---
+++ python logical operators 4.py
@@ -0,0 +1,20 @@
+#python Logical operators
+#and
+x=8
+print(x>4 and x<4)
+x=7
+print(x>5 and x<10)
+x=6
+print(x>10 and x>7)
+#or
+x=8
+print(x>4 or x<4)
+x=7
+print(x>5 or x<10)
+x=6
+print(x>10 or x>7)
+#not
+x=7
+print(not(x>3 and x<10))
+x=7
+print(not(x<3 and x<10))
