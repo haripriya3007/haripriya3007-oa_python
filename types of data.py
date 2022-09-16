43338299ecc98bad1010b5ac6b24ae37a1f44c76
haripriya3007 <113535880+haripriya3007@users.noreply.github.com>
2022-09-16 07:42:02 -0700

    Add files via upload

---
+++ types of data.py
@@ -0,0 +1,25 @@
+a="hello" #str
+b=9       #int
+c=8.0     #float
+d=True    #bool
+e={"apple","goa","cherry"}#set
+f=[6,8,9]                 #list
+g=("name","age","class")  #tuple
+h={"name":"Haripriya","age":22,"course":"python"}#dict
+i=range(6)#range
+j=6+7j#complex
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))
+print(type(h))
+print(type(i))
+print(type(j))
+
+
+
+
